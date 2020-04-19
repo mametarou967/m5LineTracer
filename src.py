fec3f901bf15a3782856095e8bacf97059cb46d8
mametarou967 <mametarou967@gmail.com>
2020-04-19 11:40:04 +0900

    センサー部分のリファクタリング

--- src.py
+++ src.py
@@ -25,18 +25,29 @@ class Display:
     if self.row >= self.rowMax:
       lcd.clear()
       self.row = 0
-    M5TextBox(0, self.row * 12, str(deltaY) + "_" + string)
+    M5TextBox(0, self.row * 12, str(self.row) + "_" + string)
     self.row = self.row + 1
 
+class Sensor:
+  
+  def __init__(self,pinin):
+    self.pinin = pinin
+  
+  def IsOnline(self):
+    if(self.pinin.value() == 1):
+      return True
+    else:
+      return False
+
 display = Display()
+sensor = Sensor(pinin)
 
 while True:
-  lineTraceValue = pinin.value()
-  display.printDisp(str(lineTraceValue))
-  if lineTraceValue == 1:
+  display.printDisp(str(sensor.IsOnline()))
+  if sensor.IsOnline() == True:
     leftover = False
     rightover = False
-  if lineTraceValue == 1 and leftover == False:
+  if sensor.IsOnline() == True and leftover == False:
     hat_BeetleC0.SetPulse(1, 120)
     wait_ms(50)
     hat_BeetleC0.SetPulse(1, 0)
@@ -44,12 +55,11 @@ while True:
   else:
     leftover = True
     wait_ms(150)
-  lineTraceValue = pinin.value()
-  display.printDisp(str(lineTraceValue))
-  if lineTraceValue == 1:
+  display.printDisp(str(sensor.IsOnline()))
+  if sensor.IsOnline() == 1:
     leftover = False
     rightover = False
-  if lineTraceValue == 1 and rightover == False:
+  if sensor.IsOnline() == True and rightover == False:
     hat_BeetleC0.SetPulse(0, 120)
     wait_ms(50)
     hat_BeetleC0.SetPulse(0, 0)
