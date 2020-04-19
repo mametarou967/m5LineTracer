48f0ef72982b7e768ef3c25d5f9fe198d5442480
mametarou967 <mametarou967@gmail.com>
2020-04-19 12:04:44 +0900

    状態の概念の追加

--- src.py
+++ src.py
@@ -5,12 +5,9 @@ import machine
 import hat
 
 setScreenColor(0x111111)
-
 hat_BeetleC0 = hat.get(hat.BEETLEC)
-
 label0 = M5TextBox(48, 1, "Line Tracer on BeetleC", lcd.FONT_Default,0xff0378, rotate=90)
 lcd.clear()
-deltaY = 0
 pinin = machine.Pin(33, mode=machine.Pin.IN, pull=machine.Pin.PULL_UP)
 leftover = False
 rigthover = False
@@ -38,32 +35,76 @@ class Sensor:
       return True
     else:
       return False
+      
+class BeetlecController:
 
+  def __init__(self,hatBeetle):
+    self.hatBeetle = hatBeetle
+    
+  def moveRight(self):
+    self.hatBeetle.SetPulse(0, -30)
+    self.hatBeetle.SetPulse(1, 120)
+    wait_ms(50)
+    self.stop()
+    wait_ms(100)
+  
+  def moveLeft(self):
+    self.hatBeetle.SetPulse(0, 120)
+    self.hatBeetle.SetPulse(1, -30)
+    wait_ms(50)
+    self.stop()
+    wait_ms(100)
+    
+  def stop(self):
+    self.hatBeetle.SetPulse(0, 0)
+    self.hatBeetle.SetPulse(1, 0)
+
+class BeetlecAutomaton:
+  
+  def __init__(self,beetlecController,Sensor):
+    self.beetlecController = beetlecController
+    self.sensor = Sensor
+    self.stateWait = 0
+    self.stateOnlineRightMove = 1
+    self.stateOnlineLeftMove = 2
+    self.stateHamidashiRight = 3
+    self.stateHamidashiLeft = 4
+    self.state = self.stateWait
+
+  def raiseEvent(self):
+    # èÛë‘ëJà⁄
+    if self.state == self.stateWait:
+      if self.sensor.IsOnline():
+        self.state = self.stateOnlineRightMove
+    elif self.state == self.stateOnlineRightMove:
+      if self.sensor.IsOnline():
+        self.state = self.stateOnlineLeftMove
+      else:
+        self.state = self.stateHamidashiRight
+    elif self.state == self.stateOnlineLeftMove:
+      if self.sensor.IsOnline():
+        self.state = self.stateOnlineRightMove
+      else:
+        self.state = self.stateHamidashiLeft
+    elif self.state == self.stateHamidashiRight:
+      if self.sensor.IsOnline():
+        self.state = self.stateOnlineLeftMove
+    elif self.state == self.stateHamidashiLeft:
+      if self.sensor.IsOnline():
+        self.state = self.stateOnlineRightMove
+    
+    self.action()
+  
+  def action(self):
+    self.beetlecController.moveRight()
+    self.beetlecController.moveLeft()
+    
 display = Display()
 sensor = Sensor(pinin)
+beetlecController = BeetlecController(hat_BeetleC0)
+beetlecAutomaton = BeetlecAutomaton(beetlecController,sensor)
 
 while True:
   display.printDisp(str(sensor.IsOnline()))
-  if sensor.IsOnline() == True:
-    leftover = False
-    rightover = False
-  if sensor.IsOnline() == True and leftover == False:
-    hat_BeetleC0.SetPulse(1, 120)
-    wait_ms(50)
-    hat_BeetleC0.SetPulse(1, 0)
-    wait_ms(100)
-  else:
-    leftover = True
-    wait_ms(150)
-  display.printDisp(str(sensor.IsOnline()))
-  if sensor.IsOnline() == 1:
-    leftover = False
-    rightover = False
-  if sensor.IsOnline() == True and rightover == False:
-    hat_BeetleC0.SetPulse(0, 120)
-    wait_ms(50)
-    hat_BeetleC0.SetPulse(0, 0)
-    wait_ms(100)
-  else:
-    leftover = True
-    wait_ms(150)
+  beetlecAutomaton.raiseEvent()
+  wait_ms(1000)
